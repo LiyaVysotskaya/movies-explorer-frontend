b0f636fd3d0e806f5f809185a2264139d1dbab2e
Liya Vysotskaya <Imredrat@yandex.ru>
2023-09-17 20:01:34 +0300

    fix: Header-logo-link height, feat: MAIN & Promo proto (17 сен 2023 г. 20:01:33)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import Header from '../Header/Header';
+import Main from '../Main/Main';
 
 import logo from '../../images/logo.svg';
 import './App.css';
 
 function App() {
+  const [loggedIn, setLoggedIn] = React.useState(false);
+
+  function handleLogin() {
+    setLoggedIn(true);
+  }
+
+  function handleLogout() {
+    setLoggedIn(false);
+    localStorage.removeItem('token');
+  }
+
   return (
     <div className="page__container">
-      <Header></Header>
+      <Header loggedIn={loggedIn} handleLogout={handleLogout} />
+      <Main></Main>
     </div>
   );
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css';
+import Promo from '../Promo/Promo';
+
+function Main(props) {
+  return (
+    <main className='main' >
+      <Promo></Promo>
+    </main>
+  )
+}
+
+export default Main;
