b4ae01468cd726c40b0849969c4e36846dc3c4c8
Liya Vysotskaya <Imredrat@yandex.ru>
2023-09-17 18:55:50 +0300

    fix: Header (17 сен 2023 г. 18:55:50)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,24 +24,40 @@ function Header(props) {
 
   function getContent() {
     if (props.loggedIn) {
-      const userEmail = localStorage.getItem('useremail');
-      return <>
-        <div className={`header__login-container ${isMenuOpen && 'header__login-container_menu'}`} onClick={closeMenu} >
-          <p className='header__email'>{userEmail}</p>
-          <Link className={`header__link-action ${isMenuOpen && 'header__link-action_menu'}`} to='/sign-in' onClick={props.handleLogout} >Выйти</Link>
-        </div>
-      </>
+      return <></>
     } else {
-      return location.pathname === '/sign-up'
-        ? <Link className='header__link-action header__link-action_bright' to='/sign-in'>Регистрация</Link>
-        : <Link className='header__link-action header__link-action_bright' to='/sign-up'>Войти</Link>
-      }
+      return (
+        <nav>
+          <ul className='header__link-container'>
+            <li>
+              <Link className='header__signup' to={'/signup'}>Регистрация</Link>
+            </li>
+            <li>
+              <Link className='header__signin' to={'/signin'}>Войти</Link>
+            </li>
+          </ul>
+        </nav>
+      )
+    }
+    // if (props.loggedIn) {
+    //   const userEmail = localStorage.getItem('useremail');
+    //   return <>
+    //     <div className={`header__login-container ${isMenuOpen && 'header__login-container_menu'}`} onClick={closeMenu} >
+    //       <p className='header__email'>{userEmail}</p>
+    //       <Link className={`header__link-action ${isMenuOpen && 'header__link-action_menu'}`} to='/sign-in' onClick={props.handleLogout} >Выйти</Link>
+    //     </div>
+    //   </>
+    // } else {
+    //   return location.pathname === '/sign-up'
+    //     ? <Link className='header__link-action header__link-action_bright' to='/sign-in'>Регистрация</Link>
+    //     : <Link className='header__link-action header__link-action_bright' to='/sign-up'>Войти</Link>
+    //   }
   }
 
   return (
-    <header className={`header centered ${isMenuOpen && props.loggedIn && 'header_colomn'}`}>
+    <header className={`header ${isMenuOpen && props.loggedIn && 'header_colomn'}`}>
       <div className='header__container'>
-        <Link className='header__link-action' to='/' ><img className="header__logo" src={logo} alt="Логотип Место" /></Link>
+        <Link className='header__link-logo' to='/' ><img className="header__logo" src={logo} alt="Логотип диплома" /></Link>
         {props.loggedIn && getMenu()}
       </div>
       {getContent()}
