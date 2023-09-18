11201ce39fba0284399308aee990b996799b393a
Liya Vysotskaya <Imredrat@yandex.ru>
2023-09-18 16:43:43 +0300

    refactor: max-width -> calc, font-family: inherit, feat: AboutProject & SecondaryHeader (18 сен 2023 г. 16:43:43)

---
+++ src/components/AboutProject/AboutProject.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './AboutProject.css';
+
+function AboutProject() {
+  return (
+    <section className="about">
+      <div className='about__header'>
+        <h2 className='about__title'>О проекте</h2>
+      </div>
+    </section>
+  )
+}
+
+export default AboutProject;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './Main.css';
 import Promo from '../Promo/Promo';
 import NavTab from '../NavTab/NavTab';
+import AboutProject from '../AboutProject/AboutProject';
 
 function Main(props) {
   return (
     <main className='main' >
       <Promo></Promo>
       <NavTab></NavTab>
+      <AboutProject></AboutProject>
     </main>
   )
 }
