fba7348e88335c7e1ae3146f3627c0bfd56d90bb
Liya Vysotskaya <Imredrat@yandex.ru>
2023-09-18 23:57:07 +0300

    fix: Footer done (18 сен 2023 г. 23:57:07)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,11 +6,13 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
-      <p className="footer__copyright">&copy; 2020</p>
-      <nav className="footer__links">
-        <Link className="footer__link">Яндекс.Практикум</Link>
-        <Link className="footer__link">Github</Link>
-      </nav>
+      <div className="footer__container">
+        <p className="footer__copyright">&copy; 2020</p>
+        <nav className="footer__links">
+          <Link className="footer__link">Яндекс.Практикум</Link>
+          <Link className="footer__link">Github</Link>
+        </nav>
+      </div>
     </footer>
   )
 }
