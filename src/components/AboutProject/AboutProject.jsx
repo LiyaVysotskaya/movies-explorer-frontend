4680ae0ebf39a3ab57658cb093f74cbc50e106e7
Liya Vysotskaya <Imredrat@yandex.ru>
2023-09-18 18:30:11 +0300

    fix: AboutProject first table, second table proto, Content margin-bottom -> 110px (18 сен 2023 г. 18:30:11)

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import './AboutProject.css';
+import SecondaryHeader from "../SecondaryHeader/SecondaryHeader";
+import Content from "../Content/Content";
 
 function AboutProject() {
   return (
     <section className="about">
-      <div className='about__header'>
-        <h2 className='about__title'>О проекте</h2>
-      </div>
+      <SecondaryHeader title='О проекте'></SecondaryHeader>
+      <Content>
+        <div className="about__container-description">
+          <h3 className="about__text">Дипломный проект включал 5 этапов</h3>
+          <h3 className="about__text">На выполнение диплома ушло 5 недель</h3>
+          <p className="about__description">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+          <p className="about__description">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </div>
+        <div className="about__container-deadlines">
+          <p className="about__time about__time-dark">1 неделя</p>
+          <p className="about__time">4 недели</p>
+          <p className="about__stage">Back-end</p>
+          <p className="about__stage">Front-end</p>
+        </div>
+      </Content>
     </section>
   )
 }
