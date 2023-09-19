9a3e2e51fd41a95740dc730879c4f45380117df9
Liya Vysotskaya <Imredrat@yandex.ru>
2023-09-19 12:03:33 +0300

    feat: adaptive start (19 сен 2023 г. 12:03:32)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,18 @@
 import React from 'react';
-import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+// import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 
-import logo from '../../images/logo.svg';
+// import logo from '../../images/logo.svg';
 import './App.css';
 import Footer from '../Footer/Footer';
 
 function App() {
-  const [loggedIn, setLoggedIn] = React.useState(false);
-
-  function handleLogin() {
-    setLoggedIn(true);
-  }
-
-  function handleLogout() {
-    setLoggedIn(false);
-    localStorage.removeItem('token');
-  }
-
   return (
     <div className="page__container">
-      <Header loggedIn={loggedIn} handleLogout={handleLogout} />
-      <Main></Main>
-      <Footer></Footer>
+      <Header />
+      {/* <Main></Main>
+      <Footer></Footer> */}
     </div>
   );
 }
