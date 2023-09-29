7ebe83c623a14cec7a891122b9a7471599a14abe
Liya Vysotskaya <Imredrat@yandex.ru>
2023-09-29 19:36:58 +0300

    feat: FilterCheckbox done (29 сен 2023 г. 19:36:58)

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import './FilterCheckbox.css';
 
-function FilterCheckbox() {
-  return(
-    <></>
+function FilterCheckbox(props) {
+  return (
+    <label className="switch">
+      <input className="switch__input" type="checkbox" value={props.isChecked} onChange={props.handleChange} />
+      <div className="swith__slider"></div>
+      <span className="swith__text">Короткометражки</span>
+    </label>
   )
 }
 
