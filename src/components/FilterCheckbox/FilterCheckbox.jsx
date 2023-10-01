913ff621092601790c132a484545bb4b0d58e3ad
Liya Vysotskaya <Imredrat@yandex.ru>
2023-10-01 12:06:38 +0300

    fix: adaptive 320px for Search, checkbox slider position ^ ( 1 окт 2023 г. 12:06:38)

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -5,8 +5,8 @@ function FilterCheckbox(props) {
   return (
     <label className="switch">
       <input className="switch__input" type="checkbox" value={props.isChecked} onChange={props.handleChange} />
-      <div className="swith__slider"></div>
-      <span className="swith__text">{props.text}</span>
+      <div className="switch__slider"></div>
+      <span className="switch__text">{props.text}</span>
     </label>
   )
 }
